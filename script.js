747f5fad188c1ae5b815b786c1b2263869ab9920
Angie Cunningham <angela.f.cunningham@gmail.com>
2017-12-16 18:31:43 +0000

    updated javascript on calculator

--- script.js
+++ script.js
@@ -1,74 +1,75 @@
+
 var keys = document.querySelectorAll("#button");
 var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
 
 
-for(var i = 0; i < keys.length; i++) {
-	keys[i].onclick = function(e) {
+for (var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(calculate) {
 
 		var input = document.querySelector('.screen');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
-		
 
-		if(btnVal == 'C') {
-			input.innerHTML = '';
+
+		if (btnVal == 'CE') {
+			input.innerHTML = '0';
 			decimalAdded = false;
 		}
-		
 
-		else if(btnVal == '=') {
+
+		else if (btnVal == '=') {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
+			//in equation variable, grab inputVal which is everything inside of input.innerHTML & then make equation.length - 1 leaves just the equation without the = sign
 			
-
-			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-			
-
-			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
-				equation = equation.replace(/.$/, '');
-			
-			if(equation)
-				input.innerHTML = eval(equation);
-				
+			input.innerHTML = eval(equation);
 			decimalAdded = false;
-		}
-		
 
-		else if(operators.indexOf(btnVal) > -1) {
-		
-	
+		}
+
+
+		else if (operators.indexOf(btnVal) > -1) {
+
+
 			var lastChar = inputVal[inputVal.length - 1];
-		
-			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+
+			if (inputVal != '' && operators.indexOf(lastChar) == -1)
 				input.innerHTML += btnVal;
-		
-			else if(inputVal == '' && btnVal == '-') 
+
+			else if (inputVal == '' && btnVal == '-')
 				input.innerHTML += btnVal;
-			
-	
-			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-		
+
+
+			if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+
 				input.innerHTML = inputVal.replace(/.$/, btnVal);
 			}
-			
-			decimalAdded =false;
-		}
-		
 
-		else if(btnVal == '.') {
-			if(!decimalAdded) {
+			decimalAdded = false;
+		}
+
+
+		else if (btnVal == '.') {
+			if (!decimalAdded) {
 				input.innerHTML += btnVal;
 				decimalAdded = true;
 			}
 		}
-		
+				
+		else if (btnVal == 'C') {
+			input.innerHTML -= btnVal;
+			// console.log(inputVal);
+			var backspace = inputVal;
+			input.innerHTML = backspace.substring(0, backspace.length - 1);
+		}
+
 
 		else {
 			input.innerHTML += btnVal;
 		}
-		
 
-		e.preventDefault();
-	} 
+
+		calculate.preventDefault();
+	}
 }
