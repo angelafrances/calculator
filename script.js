19342728b4d610f9f6094fa724ecc2233bbdc737
Angie Cunningham <angela.f.cunningham@gmail.com>
2017-12-20 00:15:00 +0000

    got my memory buttons working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var keys = document.querySelectorAll("#button");
-
+var memory = 0;
 
 for (var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(calculate) {
@@ -13,7 +13,6 @@ for (var i = 0; i < keys.length; i++) {
 			input.innerHTML = '';
 		}
 
-
 		else if (btnVal == '=') {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
@@ -24,6 +23,25 @@ for (var i = 0; i < keys.length; i++) {
 			input.innerHTML -= btnVal;
 			var backspace = inputVal;
 			input.innerHTML = backspace.substring(0, backspace.length - 1);
+		}
+		
+		else if (btnVal == 'MC') {
+			memory = 0;
+			console.log(memory);
+		}
+		
+		else if (btnVal == 'MA') {
+			memory += parseInt(inputVal);
+			console.log(memory);
+		}
+		
+		else if (btnVal == 'MS') {
+			memory -= parseInt(inputVal);
+			console.log(memory);
+		}
+		
+		else if (btnVal == 'MR') {
+			input.innerHTML = memory;
 		}
 
 
